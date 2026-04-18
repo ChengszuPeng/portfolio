ebe21daa6732265b314dbbfdf3f214fd1c17bf4b
ChengszuPeng <c7peng@ucsd.edu>
2026-04-17 18:16:38 -0700

    just a fix

--- global.js
+++ global.js
@@ -7,7 +7,7 @@ function $$(selector, context = document) {
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "/"
-    : "/PORTFOLIO/";
+    : "/portfolio/";
 
 let pages = [
   { url: "", title: "Home" },
